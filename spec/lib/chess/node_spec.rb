a29ed308298faa56baaa3cdfddbf279e87541f2d
immprada <im.mprada@gmail.com>
2022-11-02 17:01:37 -0500

    add Chess::Node specs

--- spec/lib/chess/node_spec.rb
+++ spec/lib/chess/node_spec.rb
@@ -1,5 +1,25 @@
 require './spec/spec_helper'
 
 RSpec.describe Chess::Node do
+  describe '#initialize' do
+    let(:node) { described_class.new([1, 2]) }
 
+    describe 'when the node has no parent' do
+      it 'sets the coordinates' do
+        expect(node.coordinates).to eq([1, 2])
+      end
+
+      it 'sets the parent to nil' do
+        expect(node.parent).to be_nil
+      end
+    end
+
+    describe 'when the node has a parent' do
+      let(:children) { described_class.new([3, 4], node) }
+
+      it 'sets the parent' do
+        expect(children.parent).to eq(node)
+      end
+    end
+  end
 end
