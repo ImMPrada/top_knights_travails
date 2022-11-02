7ca424793245a351b33d284e285378d08707d662
immprada <im.mprada@gmail.com>
2022-11-02 14:53:50 -0500

    add Chess::Knight .possible_move to calc coordinates of a possible position, from a position with a delta. It returns nill if possible position is out of the chess board, limited by Chess::MAX_INDEX and Chess::MIN_INDEX

--- lib/chess/knight.rb
+++ lib/chess/knight.rb
@@ -9,5 +9,16 @@ module Chess
     def initialize(coordinates)
       @from = Node.new(coordinates)
     end
+
+    private
+
+    def possible_move(position, delta)
+      move_to = [position[0] + delta[0], position[1] + delta[1]]
+
+      return nil if move_to[0] < MIN_INDEX || move_to[0] > MAX_INDEX
+      return nil if move_to[1] < MIN_INDEX || move_to[1] > MAX_INDEX
+
+      move_to
+    end
   end
 end
