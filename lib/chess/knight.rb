33f1530f437f97805663c6e854285eade563b695
immprada <im.mprada@gmail.com>
2022-11-02 14:55:25 -0500

    add Chess::Knight #move_to to recursively build a tree by breadth-first from @from position to target_position

--- lib/chess/knight.rb
+++ lib/chess/knight.rb
@@ -10,7 +10,21 @@ module Chess
       @from = Node.new(coordinates)
     end
 
+    def move_to(target_position)
+      queue = FakeQueue.new(@from)
+      last_node = move_to_helper(target_position, queue)
+    end
+
     private
+
+    def move_to_helper(target_position, queue)
+      current_node = queue.remove
+      return current_node if current_node.coordinates == target_position
+
+      possibilities = @@next_move_adds.map{ |delta| possible_move(current_node.coordinates, delta) }.compact
+      possibilities.each { |position| queue.add(Node.new(position, current_node)) }
+      move_to_helper(target_position, queue)
+    end
 
     def possible_move(position, delta)
       move_to = [position[0] + delta[0], position[1] + delta[1]]
