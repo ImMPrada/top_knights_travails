d3b8cbe98a87ab811a2797c9bc412426a49de654
immprada <im.mprada@gmail.com>
2022-11-02 17:21:19 -0500

    add Chess::Knight specs

--- spec/lib/chess/knight_spec.rb
+++ spec/lib/chess/knight_spec.rb
@@ -1,5 +1,29 @@
 require './spec/spec_helper'
 
 RSpec.describe Chess::Knight do
+  let(:starting_position) { [0, 0] }
+  let(:knight) { described_class.new(starting_position) }
 
+  describe '#initialize' do
+    it 'sets the starting position' do
+      expect(knight.instance_variable_get(:@from).coordinates).to eq(starting_position)
+    end
+
+    it 'sets the starting position to a node' do
+      expect(knight.instance_variable_get(:@from)).to be_a(Chess::Node)
+    end
+  end
+
+  describe '#move_to' do
+    let(:target_position) { [3, 3] }
+    let(:expected_path) { [[0, 0], [1, 2], [3, 3]] }
+
+    it 'returns the path to the target position' do
+      expect(knight.move_to(target_position)).to eq(expected_path)
+    end
+
+    it 'returns an array' do
+      expect(knight.move_to(target_position)).to be_a(Array)
+    end
+  end
 end
